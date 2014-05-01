c5f91b156ce4a41d3ee3b2d6eaa66aad9632e939
zhixin <wenzhixin2010@gmail.com>
2014-05-02 01:26:05 +0800

    Update header.

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,14 @@ controllers.controller('ListCtrl', ['$scope', '$http',
     function ($scope, $http) {
         $http.get('data/data.json').success(function(list) {
             $scope.scenes = list;
+
+            var count = 0;
+            for (var i = 0; i < list.length; i++) {
+                if (list[i].been) {
+                    count++;
+                }
+            }
+            $scope.count = count;
         });
     }
 ]);
